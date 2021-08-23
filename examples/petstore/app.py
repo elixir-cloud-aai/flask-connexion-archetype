4199329ecc2fcd6456fabfa44649418d37ce2fd9
Sarthak Gupta <sarthakgupta072@gmail.com>
2021-08-23 20:06:24 +0530

    feat: basic auth enabled

--- examples/petstore/app.py
+++ examples/petstore/app.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
 from foca.foca import foca
+import jwt
+
+
+def checkAuth(token):
+    print("token: ", token)
+    decodedToken = jwt.decode(token, "your-256-bit-secret", algorithms=["HS256"])
+    print(decodedToken)
+    user = decodedToken["name"]
+    print(user)
+    return { 
+        'user': user,
+        'status': '200'
+    }
+
 
 if __name__ == '__main__':
     app = foca("config.yaml")
