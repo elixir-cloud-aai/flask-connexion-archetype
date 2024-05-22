06e734499d4140898922e6aac085e687cb6ad75a
Alex Kanitz <alexander.kanitz@alumni.ethz.ch>
2024-05-23 00:24:19 +0200

    all tests pass

--- tests/security/test_cors.py
+++ tests/security/test_cors.py
@@ -1,15 +1,33 @@
 """Unit test for security.cors.py"""
 
-from unittest.mock import patch
+import functools
 
-from flask import Flask
+from connexion import FlaskApp
+from starlette.middleware.cors import CORSMiddleware
 
 from foca.security.cors import enable_cors
 
 
 def test_enable_cors():
     """Test that CORS is called with app as a parameter."""
-    app = Flask(__name__)
-    with patch('foca.security.cors.CORS') as mock_cors:
-        enable_cors(app)
-        mock_cors.assert_called_once_with(app)
+    app = FlaskApp(__name__)
+    expected_middleware = functools.partial(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    assert not any(
+        isinstance(item, functools.partial)
+        and item.func == expected_middleware.func
+        and item.args == expected_middleware.args
+        for item in app.middleware.middlewares
+    )
+    enable_cors(app)
+    assert any(
+        isinstance(item, functools.partial)
+        and item.func == expected_middleware.func
+        and item.args == expected_middleware.args
+        for item in app.middleware.middlewares
+    )
